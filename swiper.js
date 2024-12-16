a3f3c0e054dcbb4b6899b75f9098b6d9a6a0686a
santoswf <mateussantosferreira.pp@gmail.com>
2024-12-16 12:22:06 -0300

    at
    fazenda a responsividade

--- swiper.js
+++ swiper.js
@@ -1,8 +1,48 @@
 window.onscroll = function() {
   var header = document.getElementById('header');
-  if (window.scrollY > 50) {
+  if (window.scrollY > 120) {
       header.classList.add('scrolled');
   } else {
       header.classList.remove('scrolled');
   }
 };
+
+var swiper2 = new Swiper(".centered-slide-carousel", {
+  centeredSlides: true,
+  paginationClickable: true,
+  loop: true,
+  spaceBetween: 30,
+  slideToClickedSlide: true,
+  pagination: {
+    el: ".centered-slide-carousel .swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    1920: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    },
+    1028: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    990: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    }
+  }
+ });
+
+ const swiper = new Swiper('.js-testimonials-slider', {
+  grabCursor: true,
+  spaceBetween: 30,
+  pagination:{
+    el: '.js-testimonials-pagination',
+    clickable: true
+  },
+  breakpoints:{
+    767:{
+      slidesPerView: 2
+    }
+  }
+});
